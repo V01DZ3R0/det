7bd4118855a2aaa9bc55ec09e3753dd146d8074b
V01DZ3R0 <91835641+V01DZ3R0@users.noreply.github.com>
2023-10-18 22:57:48 +0300

    Update det.cpp

--- det.cpp
+++ det.cpp
@@ -2,29 +2,59 @@
 
 int main()
 {
-    int m;
+
     std::cout << "Enter matrix's rows" << std::endl;
-    std::cin >> m; // m rows
-    // детерминанта у НЕ квадратных матрицы нет по определению миша
-    if (m <= 0)
-        return 0;
-
-    int a[m][m];
-
-    for (int i = 0; i < m; i++)
+    int n;
+    std::cin >> n; // n rows & n columns
+    float a[n][n];
+    for (int i = 0; i < n; i++)
     {
         std::cout << "Enter matrix " << i + 1 << " row" << std::endl;
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             std::cin >> a[i][j];
         }
     }
-    if (m == 1)
+    if (n == 1)
     {
-        std::cout << "det:" << a[0][0] << std::endl;
+        std::cout << "det: " << a[0][0] << std::endl;
         return 0;
     }
-    
-    int det = (a[0][0] * a[1][1] * a[2][2] + a[1][0] * a[2][1] * a[0][2] + a[0][1] * a[1][2] * a[2][0]) - (a[0][2] * a[1][1] * a[2][0] + a[1][2] * a[2][1] * a[0][0] + a[0][1] * a[1][0] * a[2][2]);
-    std::cout << "det:" << det << std::endl;
+
+    if (n <= 0)
+        return 0;
+
+    //
+
+    double k;
+    for (int z = 1; z < n; z++)
+    {
+        for (int i = z; i < n; i++)
+        {
+            k = a[i][z - 1] / a[z - 1][z - 1];
+            for (int j = 0; j < n; j++)
+
+            {
+                a[i][j] -= a[z - 1][j] * k;
+            }
+        }
+    }
+
+    // std::cout << "" << std::endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         std::cout << a[i][j] * 1.0 << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    double det = 1;
+    for (int i = 0; i < n; i++)
+    {
+        det *= a[i][i];
+    }
+    std::cout << "det: " << det << std::endl;
+    return 0;
 }
